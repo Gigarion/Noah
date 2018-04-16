25bb9667626447a821dda9ff030773e82a2936c7
David Luo <eksratu@gmail.com>
2018-04-15 22:22:37 -0400

    title scene
    yee

---
+++ Noah/Assets/SelectOnInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour {
+
+    public EventSystem es;
+    public GameObject selected;
+
+    private bool buttonSelected;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false)
+        {
+            es.SetSelectedGameObject(selected);
+            buttonSelected = true;
+        }
+	}
+
+    private void OnDisable()
+    {
+        buttonSelected = false;
+    }
+}
