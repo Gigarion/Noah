abc5656b27e3a432978fe08cce0a94feea403880
David Luo <eksratu@gmail.com>
2018-04-30 21:14:59 -0400

    changed pulse bar, added rocks, cleaned up hierarchy

--- Noah/Assets/Scripts/FillOnQuest.cs
+++ Noah/Assets/Scripts/FillOnQuest.cs
@@ -6,8 +6,15 @@ using PixelCrushers.DialogueSystem;
 
 public class FillOnQuest : MonoBehaviour {
 
+    private PulseBar[] pulseBars;
+
     public void OnUse()
     {
-        gameObject.GetComponent<Slider>().value = 0.3f;
+        gameObject.GetComponent<Slider>().value = 0.4f;
+
+        // turn off pulsing
+        pulseBars = gameObject.GetComponentsInChildren<PulseBar>();
+        foreach (PulseBar p in pulseBars)
+            p.enabled = false;
     }
 }
