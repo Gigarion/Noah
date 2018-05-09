9d13b08b6946c7c24d5f68d4388cd1bd3231993b
David Luo <eksratu@gmail.com>
2018-05-08 22:05:53 -0400

    bridge ending in progress

---
+++ Noah/Assets/BreakBridge.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakBridge : MonoBehaviour
+{
+    private Spin s;
+
+    public void OnConversationEnd(Transform actor)
+    {
+        s = this.gameObject.GetComponent<Spin>();
+        s.enabled = true;
+    }
+
+}
