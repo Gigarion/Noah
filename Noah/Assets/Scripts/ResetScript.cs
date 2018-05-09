0858148af481685e61f06d3e0c827dd1a3f8701d
Matthew Parker <maddparker@gmail.com>
2018-05-09 15:53:15 -0400

    Added Reset

---
+++ Noah/Assets/Scripts/ResetScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScript : MonoBehaviour {
+
+	public static ResetScript instance;
+
+	public bool showCursor = true;
+
+	// Use this for initialization
+	void Start () {
+		if(instance == null){
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		} else {
+			Destroy(gameObject);
+		}
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Cursor.visible: " + Cursor.visible);
+
+		if(Input.GetKeyDown(KeyCode.R)){
+			SceneManager.LoadScene(0);	
+
+			//			Destroy(GameObject.Find("Dialogue Manager"));
+
+			Invoke("ToggleCursor", 0.25f);
+			//			ToggleCursor();
+		}
+	}
+
+	void ToggleCursor(){
+		Cursor.visible = showCursor;
+
+		Debug.Log("Cursor.visible: " + Cursor.visible);
+		Debug.Log(Cursor.lockState = CursorLockMode.None);
+	}
+}
