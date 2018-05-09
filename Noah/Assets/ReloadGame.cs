55c63ed1a79de9156ab00476dc62455f768b6e06
David Luo <eksratu@gmail.com>
2018-05-09 14:46:08 -0400

    ending done, changes to scene paths

---
+++ Noah/Assets/ReloadGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadGame : MonoBehaviour
+{
+    public float delay = 5;
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(0);
+    }
+}
